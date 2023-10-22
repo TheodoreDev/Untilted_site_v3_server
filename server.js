f512670a24be5a1a0244625cf42b7e6b71833ae5
TheodoreDev <theodore.delbove@gmail.com>
2023-10-22 13:07:11 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const app = express();
+const cors = require('cors')
+const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require('method-override')
+const multer = require("multer")
+
+const config = require("./config.json");
+
+const initializePassport = require('./functions/passport-config')
+initializePassport(
+  passport,
+  username => users.find(user => user.username === username),
+  id => users.find(user => user.id === id)
+)
+
+const users = []
+
+app.use(cors())
+app.use(express.json())
+app.use(flash())
+app.use(session({
+    secret: config.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+let db = new sqlite3.Database(`./Ressources/DB/${config.dbNAME}.db`, err => {
+    if(err){
+        throw err
+    }else {
+        console.log(`Database ${config.dbNAME} started.`)
+    }
+})
+db.all('SELECT * FROM users', [], (error, rows) => {
+    if(error){
+        throw error
+    }
+    rows.forEach((row) => {
+        console.log(row)
+        users.push({
+            username: row.username,
+            password: row.password,
+            email: row.email,
+            admin: row.admin,
+            id: row.id,
+            birthday: row.birthday,
+            theme: row.theme
+        })
+    })
+})
+
+app.get("/api", (req, res) => {
+    res.json({})
+})
+
+app.post('/login', async (req, res) => {
+    const user = users.find(user => user.username = req.body.username)
+    console.log(user)
+    if(await bcrypt.compare(req.body.password, user.password)) {
+        return res.json({user})
+    } else {
+        return res.json("Login Failed")
+    }
+})
+
+app.listen(config.devPort, () => {
+    console.log(`Listen on port ${config.devPort}`)
+})
